1e915dfd0c98eb6031055a3a02197ea0123d624d
Steve Atkins <steve@wordtothewise.com>
2014-02-15 11:22:30 -0800

    Maze of twisty little commit messages

--- MainWindow.cc
+++ MainWindow.cc
@@ -3,6 +3,8 @@
 #include <QTextEdit>
 #include <QPushButton>
 
+#include <stdlib.h>
+
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent), stars(0)
 {
@@ -12,8 +14,15 @@ MainWindow::MainWindow(QWidget *parent) :
   connect(east, &QPushButton::click, this, &MainWindow::doSomething);
   connect(west, &QPushButton::click, this, &MainWindow::doSomething);
   connect(inventory, &QPushButton::click, this, &MainWindow::doInventory);
-  description->setHtml(tr("<p>You've been sent on an epic quest (imagine some backstory involving an inn, a suspiciously peppy bard and a threat to middle earth as you know it) to delve deep into the tunnels beneath github, and retrieve the <s>seven</s> ten stars!</p><p>After a long and tedious journey in which we get some character origin stories, you end up in the dungeons.</p>"));
-  doPrompt();
+  description->setHtml(tr("<p>You've been sent on an epic quest (imagine"
+                          " some backstory involving an inn, a suspiciously"
+                          " peppy bard and a threat to middle earth as you"
+                          " know it) to delve deep into the tunnels beneath"
+                          " github, and retrieve the <s>seven</s> ten stars!</p>"
+                          "<p>After a long and tedious journey in which we get"
+                          " some character origin stories, you end up in the"
+                          " dungeons.</p>"));
+  prompt();
 }
 
 void MainWindow::changeEvent(QEvent *e)
@@ -30,7 +39,30 @@ void MainWindow::changeEvent(QEvent *e)
 
 void MainWindow::doSomething()
 {
+  int i = random() % 4;
+  north->setEnabled(i != 0);
+  south->setEnabled(i != 1);
+  east->setEnabled(i != 2);
+  west->setEnabled(i != 3);
 
+  QString twisty;
+  switch (i) {
+  case 0:
+    twisty = tr("twisty little maze of passages");
+    break;
+  case 1:
+    twisty = tr("maze of twisty little passages");
+    break;
+  case 2:
+    twisty = tr("little twisty maze of passages");
+    break;
+  default:
+    twisty = tr("twisty maze of little passages");
+  }
+
+  if(random() % 3) {
+
+  }
 }
 
 void MainWindow::doInventory()
